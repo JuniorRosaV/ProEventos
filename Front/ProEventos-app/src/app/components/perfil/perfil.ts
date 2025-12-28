90aad75acacf05488620529d4dcbecab0fd4ed52
Junior Rosa <alexandrejr1999@hotmail.com>
2025-12-28 15:32:18 -0300

    alterações importantes realizadas para icon e etc

--- Front/ProEventos-app/src/app/components/perfil/perfil.ts
+++ Front/ProEventos-app/src/app/components/perfil/perfil.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
-import { Titulo } from '../../shared/titulo/titulo';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [Titulo],
   templateUrl: './perfil.html',
   styleUrl: './perfil.scss',
   animations: [
     trigger('fadeUp', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('600ms ease-out', style({ opacity: 1, transform: 'none' }))
+        animate('400ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        )
       ])
     ]),
+
     trigger('slideLeft', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(-40px)' }),
-        animate('600ms 150ms ease-out')
+        style({ opacity: 0, transform: 'translateX(-30px)' }),
+        animate('400ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0)' })
+        )
       ])
     ]),
+
     trigger('slideRight', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(40px)' }),
-        animate('600ms 300ms ease-out')
+        style({ opacity: 0, transform: 'translateX(30px)' }),
+        animate('400ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0)' })
+        )
       ])
     ])
   ]
